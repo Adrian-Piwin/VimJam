b95a231c5bde06402569d094949e9f55fd4d3530
Adrian Piwin <adrian7048@gmail.com>
2020-09-22 10:12:48 -0700

    Added tv
    - Can type and remove text dynamically

---
+++ TwoDimensions/Assets/Scripts/TVController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TVController : MonoBehaviour
+{
+    public float timeBeforeRemove = 0.3f;
+    public float letterAddSpeed = 0.01f;
+    public float letterRemoveSpeed = 0.01f;
+
+    public TextMeshPro textObject;
+
+    public void displayText(string txt){
+        StartCoroutine(typeLetters(txt));
+    }
+
+    public void removeText(){
+        StartCoroutine(removeLetters());
+    }
+
+    IEnumerator typeLetters(string txt){
+        string displayText = "";
+        foreach (char c in txt){
+            yield return new WaitForSeconds(letterAddSpeed);
+            displayText = displayText + c;
+            updateText(displayText);
+        }
+
+        yield return new WaitForSeconds(timeBeforeRemove);
+        removeText();
+    }
+
+    IEnumerator removeLetters(){
+        string displayText = textObject.text;
+        for (int i = displayText.Length; i > 0; i--){
+            yield return new WaitForSeconds(letterRemoveSpeed);
+            displayText = displayText.Remove(displayText.Length - 1, 1);
+            updateText(displayText);
+        }
+    }
+
+    private void updateText(string txt){
+        textObject.SetText(txt);
+    }
+}
