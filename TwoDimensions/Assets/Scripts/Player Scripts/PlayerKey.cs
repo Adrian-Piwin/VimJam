005cc9bf755309a3dcd81971ef621b76efc889a3
Adrian Piwin <adrian7048@gmail.com>
2020-09-22 12:07:52 -0700

    Key
    - Added key
    - Player can pickup and hold "key"

---
+++ TwoDimensions/Assets/Scripts/Player Scripts/PlayerKey.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKey : MonoBehaviour
+{
+    private bool isHoldingKey;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Key"){
+            isHoldingKey = true;
+            Destroy(other.gameObject.transform.parent.gameObject);
+        }
+
+    }
+
+    public bool getKeyState(){
+        return isHoldingKey;
+    }
+
+    public void setKeyState(bool state){
+        isHoldingKey = state;
+    }
+}
---
+++ TwoDimensions/Assets/Scripts/Player Scripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    PlayerKey playerKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerKey = GetComponent<PlayerKey>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
