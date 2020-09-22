9040953e5d0bbbbf6fb92ff55901979b0388a0e3
Adrian Piwin <adrian7048@gmail.com>
2020-09-22 11:08:44 -0700

    Added follow cam

---
+++ TwoDimensions/Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject followCam;
+    public GameObject passiveCam;
+
+    public Transform leftLimit;
+    public Transform rightLimit;
+
+    private bool following = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (following)
+            passiveCam.transform.position = new Vector3(player.transform.position.x, 0, passiveCam.transform.position.z);
+        else
+            followCam.transform.position = new Vector3(player.transform.position.x, 0, followCam.transform.position.z);
+
+        if (followCam.transform.position.x <= leftLimit.position.x){
+            following = false;
+            followCam.SetActive(false);
+            passiveCam.SetActive(true);
+        }else{
+            following = true;
+            followCam.SetActive(true);
+            passiveCam.SetActive(false);
+        }
+    }
+
+
+}
